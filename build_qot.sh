a3257f300193d68c0c081f30dce94699b0545c65
Andrew Symington <andrew.c.symington@gmail.com>
2015-11-03 13:00:33 -0800

    Commit for adwait

---
+++ build_qot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Parameters
+ROOTFS=/export/rootfs
+TFTPFS=/export/tftp
+NETWAN=wlan0
+NETLAN=wpan0
+
+# Check out the submodules needed for QoT stack
+git submodule init
+git submodule update thirdparty/bb-kernel
+git submodule update thirdparty/bb.org-overlays
+
+# Pull changes to bb-kernel from upstream
+pushd thirdparty/bb.org-overlays
+git remote add upstream https://github.com/RobertCNelson/bb.org-overlays.git
+git fetch upstream
+./dtc-overlay.sh
+popd
+
+# Pull changes to bb-kernel from upstream
+pushd thirdparty/bb-kernel
+git remote add upstream https://github.com/RobertCNelson/bb-kernel.git
+git fetch upstream
+git checkout 4.1.12-bone-rt-r16
+cp ../../extra/qot.config patches/defconfig
+./build_kernel.sh
+../../extra/install_netboot.sh
+popd
