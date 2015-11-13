9bd4d1a8b62eaa01a91ad2edabf7f14c6668db2f
Andrew Symington <andrew.c.symington@gmail.com>
2015-11-12 23:15:57 -0800

    Trying E2E software timestamping as an alternative

--- module/qot_core.c
+++ module/qot_core.c
@@ -827,8 +827,10 @@ static long qot_ioctl_access(struct file *f, unsigned int cmd, unsigned long arg
 
 		// Project the time forward
 		ns = timespec64_to_ns(&ts) - binding->timeline->last;
+		pr_info("qot_core: project [a] %lld\n", ns);
 		ns = binding->timeline->nsec + ns 
 		   + div_s64(binding->timeline->mult * ns,1000000000ULL);
+	   	pr_info("qot_core: project [b] %lld\n", ns);
 		ts = ns_to_timespec64(ns);
 
 		// Send back the data structure with the updated timespec
