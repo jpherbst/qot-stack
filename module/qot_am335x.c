57304e43ba7d72e0a0c42b0189da40d61c2f94be
Andrew Symington <andrew.c.symington@gmail.com>
2016-01-12 17:36:26 -0800

    Step in the right direction (hopefully)

--- module/qot_am335x.c
+++ module/qot_am335x.c
@@ -1,27 +1,27 @@
 /*
  * @file qot_am335x.c
  * @brief QoT Driver for BBB-GPSDO in Linux 4.1.6
- * @author Andrew Symington and Fatima Anwar 
- * 
+ * @author Andrew Symington and Fatima Anwar
+ *
  * Copyright (c) Regents of the University of California, 2015. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification, 
+ * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 	1. Redistributions of source code must retain the above copyright notice, 
+ * 	1. Redistributions of source code must retain the above copyright notice,
  *     this list of conditions and the following disclaimer.
- *  2. Redistributions in binary form must reproduce the above copyright notice, 
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
  *     this list of conditions and the following disclaimer in the documentation
  *     and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 IMPORTANT : To use this driver you will need to use a special device tree overlay
@@ -44,6 +44,9 @@ IMPORTANT : To use this driver you will need to use a special device tree overla
 #include <linux/timecounter.h>
 #include <linux/rbtree.h>
 
+// We are going to export the whol am335x as a PTP clock
+#include <linux/ptp_clock_kernel.h>
+
 // DMTimer Code specific to the AM335x
 #include "/export/bb-kernel/KERNEL/arch/arm/plat-omap/include/plat/dmtimer.h"
 
@@ -51,7 +54,298 @@ IMPORTANT : To use this driver you will need to use a special device tree overla
 #include "qot.h"
 #include "qot_core.h"
 
-// FORWARD DELCLARATIONS /////////////////////////////////////////////////////////
+// PLATFORM DATA ///////////////////////////////////////////////////////////////
+
+struct qot_am335x_pdata {
+	spinlock_t lock;					/* Protects timer registers */
+	struct ptp_clock *clk;				/* PTP clock */
+	struct ptp_clock_info clkinfo;		/* PTP clock info */
+	struct clocksource clksrc;			/* Linux Clock source */
+};
+
+// PTP ABSTRACTION /////////////////////////////////////////////////////////////
+
+static struct ptp_pin_desc qot_am335x_pins[4] = {
+	{
+		.name = "AM335X_GPIO0",
+		.index = 0,
+		.func = PTP_PF_NONE | PTP_PF_PEROUT | PTP_PF_EXTTS,
+		.chan = 0
+	},
+	{
+		.name = "AM335X_GPIO1",
+		.index = 1,
+		.func = PTP_PF_NONE | PTP_PF_PEROUT | PTP_PF_EXTTS,
+		.chan = 1
+	},
+	{
+		.name = "AM335X_GPIO2",
+		.index = 2,
+		.func = PTP_PF_NONE | PTP_PF_PEROUT | PTP_PF_EXTTS,
+		.chan = 2
+	},
+	{
+		.name = "AM335X_GPIO3",
+		.index = 3,
+		.func = PTP_PF_NONE | PTP_PF_PEROUT | PTP_PF_EXTTS,
+		.chan = 3
+	}
+};
+
+static struct ptp_clock_info qot_am335x_info = {
+	.owner		= THIS_MODULE,
+	.name		= "AM335x timer",
+	.max_adj	= 1000000,
+	.n_alarm    = 0,
+	.n_ext_ts	= 4,
+	.n_pins		= 4,
+	.pps		= 0,
+	.adjfreq	= qot_am335x_adjfreq,
+	.adjtime	= qot_am335x_adjtime,
+	.gettime64	= qot_am335x_gettime,
+	.settime64	= qot_am335x_settime,
+	.enable		= qot_am335x_enable,
+};
+
+static int qot_am335x_adjfreq(struct ptp_clock_info *ptp, s32 delta)
+{
+	u64 adj;
+	u32 diff, tmr_add;
+	int neg_adj = 0;
+	struct qot_am335x *pdata = container_of(ptp, struct qot_am335x, info);
+	if (ppb < 0) {
+		neg_adj = 1;
+		ppb = -ppb;
+	}
+	tmr_add = pdata->tmr_add;
+	adj = tmr_add;
+	adj *= ppb;
+	diff = div_u64(adj, 1000000000ULL);
+	tmr_add = neg_adj ? tmr_add - diff : tmr_add + diff;
+	gfar_write(&pdata->regs->tmr_add, tmr_add);
+	return 0;
+}
+
+static int qot_am335x_adjtime(struct ptp_clock_info *ptp, s64 delta)
+{
+	s64 now;
+	unsigned long flags;
+	struct qot_am335x_data *pdata = container_of(ptp, struct qot_am335x_data, info);
+
+	spin_lock_irqsave(&pdata->lock, flags);
+
+	now = tmr_cnt_read(pdata);
+	now += delta;
+	tmr_cnt_write(pdata, now);
+
+	spin_unlock_irqrestore(&pdata->lock, flags);
+
+	set_fipers(pdata);
+	return 0;
+}
+
+static int qot_am335x_gettime64(struct ptp_clock_info *ptp,
+                                struct timespec64 *ts)
+{
+	u64 ns;
+	unsigned long flags;
+	struct etsects *etsects = container_of(ptp, struct etsects, caps);
+
+	spin_lock_irqsave(&etsects->lock, flags);
+
+	ns = tmr_cnt_read(etsects);
+
+	spin_unlock_irqrestore(&etsects->lock, flags);
+
+	*ts = ns_to_timespec64(ns);
+
+	return 0;
+}
+
+static int qot_am335x_settime64(struct ptp_clock_info *p,
+                                const struct timespec64 *ts)
+{
+	u64 ns;
+	unsigned long flags;
+	struct etsects *etsects = container_of(ptp, struct etsects, caps);
+
+	ns = timespec64_to_ns(ts);
+
+	spin_lock_irqsave(&etsects->lock, flags);
+
+	tmr_cnt_write(etsects, ns);
+	set_fipers(etsects);
+
+	spin_unlock_irqrestore(&etsects->lock, flags);
+}
+
+static int qot_am335x_enable(struct ptp_clock_info *ptp,
+                             struct ptp_clock_request *rq, int on)
+{
+	struct am335x_data *pdata = container_of(ptp, struct am335x_data, info);
+	unsigned long flags;
+	u32 bit, mask;
+
+	switch (rq->type) {
+
+	// Configure input timestamping
+	case PTP_CLK_REQ_EXTTS:
+		switch (rq->extts.index) {
+		case 0: case 1: case 2: case 3:
+			if (qot_am335x_timer_extts(pdata->timer_gpio[rq->extts.index])
+			    return -EINVAL;
+			    break;
+		    default:
+				    return -EINVAL;
+			}
+		return 0;
+
+	// Configure external interrupt generation
+	case PTP_CLK_REQ_PEROUT:
+	switch (rq->perout.index) {
+		case 0: case 1: case 2: case 3:
+			if (qot_am335x_timer_perout(pdata->timer_gpio[rq->perout.index])
+			    return -EINVAL;
+			if (on) {
+				u64 start_ns, period_ns;
+				start_ns = rq->perout.start.sec * 1000000000ULL
+				           + rq->perout.start.nsec;
+				period_ns = rq->perout.period.sec * 1000000000ULL
+				            + rq->perout.period.nsec;
+				if (qot_am335x_period_on(pdata->timer_gpio[rq->perout.index],
+					                         start_ns, period_ns)
+					    return -EINVAL;
+				}
+		break;
+
+	//
+	default:
+			return -EINVAL;
+	}
+	return 0;
+
+default:
+	break;
+}
+
+return -EOPNOTSUPP;
+}
+
+static int qot_am335x_verify(struct ptp_clock_info *ptp, unsigned int pin,
+                             enum ptp_pin_function func, unsigned int chan)
+{
+   	// Check that the pin exists
+	if (pin >= ptp->n_pins || !ptp->pin_config)
+        return -EINVAL;
+
+	// User cannot change the channel
+	if (chan != ptp->pin_config[pin].chan)
+        return -EINVAL;
+
+	// We dont support PHY sync
+	if (func == PTP_PF_PHYSYNC)
+        return -EINVAL;
+
+	return 0;
+}
+
+// DEVICE TREE PARSING /////////////////////////////////////////////////////////
+
+static struct qot_am335x_data *qot_am335x_of_parse(struct platform_device *pdev)
+{
+	struct qot_am335x_data *pdata = NULL;
+	struct clk *gt_fclk;;
+
+	// Try and allocate some memory
+	pdata = devm_kzalloc(&pdev->dev, sizeof(struct qot_am335x_data), GFP_KERNEL);
+	if (!pdata)
+		return NULL;
+
+	// Create the clocksource from which to drive linux
+	gt_fclk = omap_dm_timer_get_fclk(pdata->timer_core);
+	pdata->frequency = clk_get_rate(gt_fclk);
+	pdata->clksrc.name = "qot";
+	pdata->clksrc.rating = 299;
+	pdata->clksrc.read = roseline_read_cycles_timer_a;
+	pdata->clksrc.mask = CLOCKSOURCE_MASK(32);
+	pdata->clksrc.flags = CLOCK_SOURCE_IS_CONTINUOUS;
+	if (clocksource_register_hz(&pdata->clksrc, pdata->frequency))
+		pr_err("Could not register clocksource %s\n", pdata->clksrc.name);
+	else
+		pr_info("clocksource: %s at %u Hz\n", pdata->clksrc.name, pdata->frequency);
+
+}
+
+static void roseline_clocksource_cleanup(struct roseline_clock_data *pdata)
+{
+	clocksource_unregister(&pdata->clksrc);
+}
+
+// Return th platform data
+return pdata;
+}
+
+
+// MODULE LOADING AND UNLOADING  ///////////////////////////////////////////////
+
+static const struct of_device_id qot_am335x_dt_ids[] = {
+	{ .compatible = "qot_am335x", },
+	{ }
+};
+
+MODULE_DEVICE_TABLE(of, qot_am335x_dt_ids);
+
+static int qot_am335x_probe(struct platform_device *pdev)
+{
+	struct qot_am335x_pin *pin;
+	struct rb_node *node;
+	const struct of_device_id *match = of_match_device(
+	                                       qot_am335x_dt_ids, &pdev->dev);
+	if (match) {
+		pdev->dev.platform_data = qot_am335x_of_parse(pdev);
+		if (!pdata)
+			return -ENODEV;
+	} else {
+		pr_err("of_match_device failed\n");
+		return -ENODEV;
+	}
+	return 0;
+}
+
+static int qot_am335x_remove(struct platform_device *pdev)
+{
+	struct qot_am335x_pin *pin;
+	struct rb_node *node;
+	if (pdev->dev.platform_data) {
+		clocksource_unregister(&pdata->clksrc);
+		devm_kfree(&pdev->dev, pdev->dev.platform_data);
+		pdev->dev.platform_data = NULL;
+	}
+	platform_set_drvdata(pdev, NULL);
+	return 0;
+}
+
+static struct platform_driver qot_am335x_driver = {
+	.probe    = qot_am335x_probe,
+	.remove   = qot_am335x_remove,
+	.driver   = {
+		.name = MODULE_NAME,
+		.owner = THIS_MODULE,
+		.of_match_table = of_match_ptr(qot_am335x_dt_ids),
+	},
+};
+
+module_platform_driver(qot_am335x_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Andrew Symington");
+MODULE_DESCRIPTION("Linux PTP Driver for AM335x");
+MODULE_VERSION("0.5.0");
+
+
+/*
+
+// FORWARD DELCLARATIONS ///////////////////////////////////////////////////////
 
 // Module information
 #define MODULE_NAME 			"qot_am335x"
@@ -254,19 +548,19 @@ static void qot_am335x_compare_start(struct qot_am335x_pin *pin)
 	// First, using pdata->tc and pdata->cc, determine if the starting edge (pdata->start) is
 	// expires some time between now and the end of this overflow period 0xFFFFFFFF. Then, if
 	// true, calculate the exact poiny (cycle) that corresponds to the starting edge.
-	/*
-	mintime = timecounter_read(&pdata->tc);
-	maxtime = mintime + (((cycle_t) 0xFFFFFFFF) * pdata->tc.cc->mult) >> pdata->tc.cc->shift;	
-	pr_info("qot_am335x: MINTIME %llu\n",mintime);
-	pr_info("qot_am335x: MAXTIME %llu\n",maxtime);
-	pr_info("qot_am335x: START   %llu\n",pin->start);
-	if (pin->start < mintime || pin->start > maxtime)
-	{
-		pr_info("qot_am335x: PWM deferred");	
-		return;
-	}
-	pr_info("qot_am335x: PWM triggered");
-	*/
+
+	// mintime = timecounter_read(&pdata->tc);
+	// maxtime = mintime + (((cycle_t) 0xFFFFFFFF) * pdata->tc.cc->mult) >> pdata->tc.cc->shift;
+	// pr_info("qot_am335x: MINTIME %llu\n",mintime);
+	// pr_info("qot_am335x: MAXTIME %llu\n",maxtime);
+	// pr_info("qot_am335x: START   %llu\n",pin->start);
+	// if (pin->start < mintime || pin->start > maxtime)
+	// {
+	// 	pr_info("qot_am335x: PWM deferred");
+	// 	return;
+	// }
+	// pr_info("qot_am335x: PWM triggered");
+
 
 	// Get the cycle at which the PWM must be triggered
 	c_cycle = (uint64_t) pdata->cc.read(&pdata->cc);
@@ -279,7 +573,7 @@ static void qot_am335x_compare_start(struct qot_am335x_pin *pin)
 	// Configure PWM
 	load  = -(pin->high + pin->low);
 	match = -(pin->low);
-	value = __omap_dm_timer_read(pdata->timer, OMAP_TIMER_COUNTER_REG, 
+	value = __omap_dm_timer_read(pdata->timer, OMAP_TIMER_COUNTER_REG,
 		pdata->timer->posted) - (uint32_t) m_cycle + AM335X_KLUDGE_FACTOR;
 	__omap_dm_timer_write(pin->timer, OMAP_TIMER_COUNTER_REG, value, pin->timer->posted);
 	__omap_dm_timer_write(pin->timer, OMAP_TIMER_LOAD_REG, load, pin->timer->posted);
@@ -290,7 +584,7 @@ static void qot_am335x_compare_start(struct qot_am335x_pin *pin)
 }
 
 // We might have a capture request scheduled beyond the next interrupt. So, we have to check on
-// each timer overflow that 
+// each timer overflow that
 static void qot_am335x_compare_stop(struct qot_am335x_pin *pin)
 {
 	__omap_dm_timer_write(pin->timer, OMAP_TIMER_LOAD_REG,  0,  pin->timer->posted);
@@ -311,7 +605,7 @@ static void qot_am335x_capture_push(struct work_struct *ws)
 		qot_push_capture(work->pin->name, timecounter_cyc2time(&pdata->tc,work->edge));
 		kfree(work);
 	}
-} 
+}
 
 // Interrupt handler for compare timers
 static irqreturn_t qot_am335x_interrupt(int irq, void *data)
@@ -321,8 +615,8 @@ static irqreturn_t qot_am335x_interrupt(int irq, void *data)
 	struct qot_am335x_pin *pin = data;
 	struct qot_am335x_work *work;
 
-	spin_lock_irqsave(&pdata->lock, flags);	
-	
+	spin_lock_irqsave(&pdata->lock, flags);
+
 	// If the OMAP_TIMER_INT_CAPTURE flag in the IRQ status is set
 	irq_status = omap_dm_timer_read_status(pin->timer);
 
@@ -338,7 +632,7 @@ static irqreturn_t qot_am335x_interrupt(int irq, void *data)
 			{
 				INIT_WORK(&work->work, qot_am335x_capture_push);
 				work->pin = pin;
-				work->edge = __omap_dm_timer_read(pin->timer, 
+				work->edge = __omap_dm_timer_read(pin->timer,
 					OMAP_TIMER_CAPTURE_REG, pin->timer->posted);
 				queue_work(pdata->wq, &work->work);
 			}
@@ -354,7 +648,7 @@ static irqreturn_t qot_am335x_interrupt(int irq, void *data)
 		// Don't bother checking  if this is not a compare timer
    		if (pin && pin->type == AM335X_TYPE_COMPARE)
    		{
-			// If we have reached the final cycle, stop 
+			// If we have reached the final cycle, stop
 			if (pin->repeat == 1)
 				qot_am335x_compare_stop(pin);
 
@@ -379,7 +673,7 @@ static irqreturn_t qot_am335x_interrupt(int irq, void *data)
 	}
 
 	spin_unlock_irqrestore(&pdata->lock, flags);
-	
+
 	return IRQ_HANDLED;
 }
 
@@ -439,7 +733,7 @@ static struct qot_am335x_data *qot_am335x_of_parse(struct platform_device *pdev)
 	{
 		pr_err("qot_am335x: could not find phandle for core");
 		return NULL;
-	} 
+	}
 	if (of_parse_phandle_with_fixed_args(np, "core", 1, 0, &timer_args) < 0)
 	{
 		pr_err("qot_am335x: could not parse core timer arguments\n");
@@ -497,7 +791,7 @@ static struct qot_am335x_data *qot_am335x_of_parse(struct platform_device *pdev)
 	timecounter_init(&pdata->tc, &pdata->cc, 0);
 	INIT_DELAYED_WORK(&pdata->overflow_work, qot_am335x_overflow_check);
 	schedule_delayed_work(&pdata->overflow_work, AM335X_OVERFLOW_PERIOD);
-	
+
 	// Setup the red-black tree root
 	pdata->pin_root = RB_ROOT;
 
@@ -512,14 +806,14 @@ static struct qot_am335x_data *qot_am335x_of_parse(struct platform_device *pdev)
 		{
 			pr_err("qot_am335x: cannot find phandle for timer%d", i);
 			continue;
-		} 
+		}
 		if (of_parse_phandle_with_fixed_args(np, name, 1, 0, &timer_args) < 0)
 		{
 			pr_err("qot_am335x: could not parse core timer%d arguments",i);
 			continue;
 		}
 		timer_node = of_find_node_by_phandle(be32_to_cpup(phandle));
-		
+
 		tmp = NULL;
 		of_property_read_string_index(timer_node, "ti,hwmods", 0, &tmp);
 		if (!tmp)
@@ -561,7 +855,7 @@ static struct qot_am335x_data *qot_am335x_of_parse(struct platform_device *pdev)
 
 		// Set the pin type
 		pin->type = AM335X_TYPE_CAPTURE;
-		
+
 		// Set the interrupt typw
 		timer_irqcfg = OMAP_TIMER_CTRL_TCM_LOWTOHIGH;
 		switch (timer_args.args[0])
@@ -587,7 +881,7 @@ static struct qot_am335x_data *qot_am335x_of_parse(struct platform_device *pdev)
 
 		// Align the hardware count of this timer as best as possible to the core timer. Since they
 		// are clocked by the same oscillator, there should be no relative drift.
-		__omap_dm_timer_write(timer, OMAP_TIMER_COUNTER_REG, __omap_dm_timer_read(pdata->timer, 
+		__omap_dm_timer_write(timer, OMAP_TIMER_COUNTER_REG, __omap_dm_timer_read(pdata->timer,
 			OMAP_TIMER_COUNTER_REG, pdata->timer->posted) + AM335X_KLUDGE_FACTOR, timer->posted);
 
 		// Add the pin to the list
@@ -596,7 +890,7 @@ static struct qot_am335x_data *qot_am335x_of_parse(struct platform_device *pdev)
 		// Put the device tree node back
 		of_node_put(timer_node);
 	}
-	
+
 	// Return th platform data
 	return pdata;
 }
@@ -604,7 +898,7 @@ static struct qot_am335x_data *qot_am335x_of_parse(struct platform_device *pdev)
 // CALLBACK STRUCT ///////////////////////////////////////////////////////////
 
 // Setup the compare pin
-int qot_am335x_setup_compare(const char *name, uint8_t enable, 
+int qot_am335x_setup_compare(const char *name, uint8_t enable,
 	uint64_t start, uint32_t high, uint32_t low, uint64_t repeat)
 {
 	// Search for the timer with the supplied name
@@ -628,7 +922,7 @@ int qot_am335x_setup_compare(const char *name, uint8_t enable,
 
 	// Check to see if this needs to be scheduled
 	qot_am335x_compare_start(pin);
-	
+
 	// Success!
 	return 0;
 }
@@ -658,7 +952,7 @@ static int qot_am335x_probe(struct platform_device *pdev)
 {
 	struct qot_am335x_pin *pin;
 	struct rb_node *node;
-	const struct of_device_id *match = 
+	const struct of_device_id *match =
 		 of_match_device(qot_am335x_dt_ids, &pdev->dev);
 	if (match)
 	{
@@ -716,7 +1010,7 @@ static int qot_am335x_remove(struct platform_device *pdev)
 
 			// Cleanup a pin timer
 			omap_dm_timer_set_source(pin->timer, OMAP_TIMER_SRC_SYS_CLK);
-			omap_dm_timer_set_int_disable(pin->timer, 
+			omap_dm_timer_set_int_disable(pin->timer,
 				OMAP_TIMER_INT_MATCH | OMAP_TIMER_INT_CAPTURE | OMAP_TIMER_INT_OVERFLOW);
 			free_irq(pin->timer->irq, pin);
 			omap_dm_timer_stop(pin->timer);
@@ -757,7 +1051,7 @@ static struct platform_driver qot_am335x_driver = {
   .probe    = qot_am335x_probe,
   .remove   = qot_am335x_remove,
 	.driver   = {
-		.name = MODULE_NAME,			
+		.name = MODULE_NAME,
 		.owner = THIS_MODULE,
 		.of_match_table = of_match_ptr(qot_am335x_dt_ids),
 	},
@@ -769,3 +1063,5 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Andrew Symington");
 MODULE_DESCRIPTION("QoT Driver for AM335x in Linux 4.1.x");
 MODULE_VERSION("0.5.0");
+
+*/
