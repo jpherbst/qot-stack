7f909f4645dffecd6b70eb7ef1ae0a936724158f
Andrew Symington <andrew.c.symington@gmail.com>
2016-02-18 09:33:52 -0800

    Updated BBB script

--- targets/build_beaglebone_black.sh
+++ targets/build_beaglebone_black.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo Not yet implemented.
+pushd am335x
+dtc -O dtb -o BBB-AM335X-00A0.dtbo -b 0 -@ BBB-AM335X-00A0.dts
+scp BBB-AM335X-00A0.dtbo root@quadrotor_controller:/export/rootfs/lib/firmware
+scp ../../src/modules/qot_am335x/qot_am335x.ko root@quadrotor_controller:/export/rootfs/lib/modules/4.1.12-bone-rt-r16/kernel/drivers/misc/
+ssh root@bbb-bravo depmod
