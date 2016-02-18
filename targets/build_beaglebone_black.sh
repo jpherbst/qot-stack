0b67572830861600a0a20332e86b11bb7af69e3d
Andrew Symington <andrew.c.symington@gmail.com>
2016-02-18 15:35:45 -0800

    Small script fixups for testing

--- targets/build_beaglebone_black.sh
+++ targets/build_beaglebone_black.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+	echo "usage: build_beaglebone_black.sh root@host"
+	exit 1
+fi
 pushd am335x
 dtc -O dtb -o BBB-AM335X-00A0.dtbo -b 0 -@ BBB-AM335X-00A0.dts
-scp BBB-AM335X-00A0.dtbo root@quadrotor_controller:/export/rootfs/lib/firmware
-scp ../../src/modules/qot_am335x/qot_am335x.ko root@quadrotor_controller:/export/rootfs/lib/modules/4.1.12-bone-rt-r16/kernel/drivers/misc/
-ssh root@bbb-bravo depmod
+scp BBB-AM335X-00A0.dtbo $1:/lib/firmware
+scp ../../src/modules/qot_am335x/qot_am335x.ko $1:/lib/modules/4.1.12-bone-rt-r16/kernel/drivers/misc/
+ssh $1 depmod
