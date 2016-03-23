09c4f68dd8d61562d5f5f65ede144b977db267da
fatimanwar <flairyparagon@gmail.com>
2016-03-23 15:51:37 -0700

    -added new ioctl to timeline char device, basically to access highest acc and res requirements
    -fixed helloworld to add resolution and accuracy values

--- src/qot_types.h
+++ src/qot_types.h
@@ -468,12 +468,13 @@ typedef struct qot_binding {
  */
 #define TIMELINE_MAGIC_CODE 0xEF
 #define TIMELINE_GET_INFO           _IOR(TIMELINE_MAGIC_CODE, 1, qot_timeline_t*)
-#define TIMELINE_BIND_JOIN         _IOWR(TIMELINE_MAGIC_CODE, 2, qot_binding_t*)
-#define TIMELINE_BIND_LEAVE         _IOW(TIMELINE_MAGIC_CODE, 3, qot_binding_t*)
-#define TIMELINE_BIND_UPDATE        _IOW(TIMELINE_MAGIC_CODE, 4, qot_binding_t*)
-#define TIMELINE_CORE_TO_REMOTE    _IOWR(TIMELINE_MAGIC_CODE, 5, timepoint_t*)
-#define TIMELINE_REMOTE_TO_CORE    _IOWR(TIMELINE_MAGIC_CODE, 6, timepoint_t*)
-#define TIMELINE_GET_TIME_NOW       _IOR(TIMELINE_MAGIC_CODE, 7, utimepoint_t*)
-#define TIMELINE_SLEEP_UNTIL       _IOWR(TIMELINE_MAGIC_CODE, 8, utimepoint_t*)
+#define TIMELINE_GET_BINDING_INFO	_IOR(TIMELINE_MAGIC_CODE, 2, qot_binding_t*)
+#define TIMELINE_BIND_JOIN         _IOWR(TIMELINE_MAGIC_CODE, 3, qot_binding_t*)
+#define TIMELINE_BIND_LEAVE         _IOW(TIMELINE_MAGIC_CODE, 4, qot_binding_t*)
+#define TIMELINE_BIND_UPDATE        _IOW(TIMELINE_MAGIC_CODE, 5, qot_binding_t*)
+#define TIMELINE_CORE_TO_REMOTE    _IOWR(TIMELINE_MAGIC_CODE, 6, timepoint_t*)
+#define TIMELINE_REMOTE_TO_CORE    _IOWR(TIMELINE_MAGIC_CODE, 7, timepoint_t*)
+#define TIMELINE_GET_TIME_NOW       _IOR(TIMELINE_MAGIC_CODE, 8, utimepoint_t*)
+#define TIMELINE_SLEEP_UNTIL       _IOWR(TIMELINE_MAGIC_CODE, 9, utimepoint_t*)
 
 #endif
