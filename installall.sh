9edc6fadeab996c56e06535b847a5968f435d728
Sean Kim <sean.hk127@gmail.com>
2016-08-25 11:34:48 -0700

    added 'test' to bbbcontrol and allow IP args to installall

--- installall.sh
+++ installall.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+# usage:
+# ./installall.sh [IP addresses..]
+# if no argument, it installs on all IPADDRESSES variable
+
 IPADDRESSES="bbb-alpha bbb-bravo bbb-charlie"
 
+# If there are arguments then replace IPADDRESSES list
+if [ $# -ne 0 ]
+then
+	IPADDRESSES=""
+	for arg in "$@"
+	do
+		IPADDRESSES+=" $arg"
+	done
+	echo "IPADDRESSES=$IPADDRESSES"
+fi
+
+# Run in background for each IP address
 for IP in $IPADDRESSES
 do
 	(make install_sd IPADDR=$IP >/dev/null 2>&1) &
@@ -9,6 +25,7 @@ do
 	pids+=" $!"
 done
 
+# Wait for each install process, reporting return value
 for p in $pids;
 do
 	if wait $p; then
